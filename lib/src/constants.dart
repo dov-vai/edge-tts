fa12a18f0f15553d52323102e65dce925956f908
dov-vai <160327869+dov-vai@users.noreply.github.com>
2024-09-22 21:56:15 +0300

    feat: port constants

---
+++ lib/src/constants.dart
@@ -0,0 +1,9 @@
+const String trustedClientToken = "6A5AA1D4EAFF4E9FB37E23D68491D6F4";
+
+const String wssUrl =
+    "wss://speech.platform.bing.com/consumer/speech/synthesize/"
+    "readaloud/edge/v1?TrustedClientToken=$trustedClientToken";
+
+const String voiceListUrl =
+    "https://speech.platform.bing.com/consumer/speech/synthesize/"
+    "readaloud/voices/list?trustedclienttoken=$trustedClientToken";
